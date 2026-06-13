438ffe31d7d1256dece36375e4bde9bca9fa698e
Ashish Patil <contactashishpatil@gmail.com>
2026-06-13 17:27:34 +0530

    Created Lang Graph E2E workflow

---
+++ graphs/review_graph.py
@@ -0,0 +1,168 @@
+from models.analysis_state import AnalysisState
+from langgraph.graph import StateGraph
+from services.terraform_parser import parse_terraform
+from services.security_checks import run_security_checks
+from services.terraform_reviewer import review_terraform
+from services.deduplication import deduplicate_findings
+from services.risk_scoring import calculate_score
+from services.test_generator import generate_tests
+
+def parse_node(state: AnalysisState):
+
+    state.parsed_terraform = parse_terraform(
+        state.terraform_code
+    )
+
+    print(
+        "Parsed:",
+        state.parsed_terraform
+    )
+
+    return state
+
+def security_node(state: AnalysisState):
+
+    findings = run_security_checks(
+        state.terraform_code,
+        state.parsed_terraform
+    )
+
+    state.findings.extend(findings)
+
+    print(
+        "Security Findings:",
+        len(state.findings)
+    )
+
+    return state
+
+def review_node(state: AnalysisState):
+
+    findings = review_terraform(
+        state.terraform_code
+    )
+
+    state.findings.extend(
+        findings
+    )
+
+    print(
+        "Total Findings After AI:",
+        len(state.findings)
+    )
+
+    return state
+
+def dedup_node(state: AnalysisState):
+
+    before = len(state.findings)
+
+    state.findings = deduplicate_findings(
+        state.findings
+    )
+
+    after = len(state.findings)
+
+    print(
+        f"Deduplicated: {before} -> {after}"
+    )
+
+    return state
+
+def score_node(state: AnalysisState):
+
+    for finding in state.findings:
+        finding.score = calculate_score(
+            finding.severity
+        )
+
+    state.total_score = sum(
+        finding.score
+        for finding in state.findings
+    )
+
+    print(
+        "Risk Score:",
+        state.total_score
+    )
+
+    return state
+
+def test_generation_node(
+    state: AnalysisState
+):
+    state.generated_tests = generate_tests(
+        state.terraform_code
+    )
+
+    print(
+        "Generated Tests"
+    )
+
+    return state
+
+builder = StateGraph(AnalysisState)
+
+builder.add_node(
+    "parse",
+    parse_node
+)
+
+builder.add_node(
+    "security",
+    security_node
+)
+
+builder.add_node(
+    "review",
+    review_node
+)
+
+builder.add_node(
+    "dedup",
+    dedup_node
+)
+
+builder.add_node(
+    "score",
+    score_node
+)
+
+builder.add_node(
+    "generate_tests",
+    test_generation_node
+)
+
+builder.add_edge(
+    "parse",
+    "security"
+)
+
+builder.add_edge(
+    "security",
+    "review"
+)
+
+builder.add_edge(
+    "review",
+    "dedup"
+)
+
+builder.add_edge(
+    "dedup",
+    "score"
+)
+
+builder.add_edge(
+    "score",
+    "generate_tests"
+)
+
+builder.set_entry_point(
+    "parse"
+)
+builder.set_finish_point(
+    "generate_tests"
+)
+graph = builder.compile()
+
--- reviewer.py
+++ reviewer.py
@@ -1,16 +1,8 @@
-import json
-from typing import List
-from openai import OpenAI
 from dotenv import load_dotenv
-from services.terraform_reviewer import review_terraform
 import argparse
-from services.security_checks import run_security_checks
-from services.risk_scoring import calculate_score
-from services.deduplication import deduplicate_findings
-from models.finding import Finding
-import services.test_generator
 from models.analysis_state import AnalysisState
-from services.terraform_parser import parse_terraform
+from graphs.review_graph import graph
+
 load_dotenv()
 
 parser = argparse.ArgumentParser()
@@ -28,50 +20,7 @@ with open(args.terraform_file, "r") as f:
 state = AnalysisState(
     terraform_code=terraform_code
 )
+result = graph.invoke(state)
 
-state.parsed_terraform = parse_terraform(
-    state.terraform_code
-)
+print(result)
 
-
-state.findings.extend(
-    run_security_checks(state.terraform_code, state.parsed_terraform)
-)
-
-state.findings.extend(
-    review_terraform(state.terraform_code)
-)
-
-state.findings = deduplicate_findings(
-    state.findings
-)
-
-for finding in state.findings:
-    finding.score = calculate_score(
-        finding.severity
-    )
-
-state.total_score = sum(
-    finding.score
-    for finding in state.findings
-)
-
-for finding in state.findings:
-    print(
-        f"[{finding.severity}] "
-        f"{finding.title}"
-    )
-
-print("\nTotal Risk Score:", state.total_score)
-
-
-
-state.generated_tests = (
-    services.test_generator.generate_tests(
-        state.terraform_code
-    )
-)
-
-print("\nGenerated Terratest:")
-print("=" * 40 + "\n")
-print(state.generated_tests)
